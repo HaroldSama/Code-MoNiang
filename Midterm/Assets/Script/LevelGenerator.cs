9e3e6105d730a33953566da26569f4d879011c6f
177sma <howerds4my@gmail.com>
2019-04-02 22:21:47 -0400

    Initiate

---
+++ Midterm/Assets/Script/LevelGenerator.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelGenerator : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void Start()
+    {
+        string filePath = Application.dataPath + "/Files/level" + GameManager.Instance.level + ".txt";
+
+        if (!File.Exists(filePath))
+        {
+            File.WriteAllText(filePath, "XXXXXXXXXXXXXXXXXXXXXXXX\n" +
+                                                 "X----------------------X\n" +
+                                                 "X----------------------X\n" +
+                                                 "X-----------G----------X\n" +
+                                                 "X----------------------X\n" +
+                                                 "X----------------------X\n" +
+                                                 "X--P-------------------X\n" +
+                                                 "X----------------------X\n" +
+                                                 "X----------------------X\n" +
+                                                 "XXXXXXXXXXXXXXXXXXXXXXXX\n");
+        }
+
+        string[] inputLines = File.ReadAllLines(filePath);
+
+        for (int y = 0; y < inputLines.Length; y++)
+        {
+            string line = inputLines[inputLines.Length - 1 - y];
+
+            for (int x = 0; x < line.Length; x++)
+            {
+                if (line[x] != '-')
+                {
+                    GameObject tile = Instantiate(Resources.Load("Prefabs/" + line[x]), gameObject.transform) as GameObject;
+                    tile.transform.position = new Vector2(x - 11.5f, y - 5);                
+                }
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+}
+    
---
+++ Midterm/Assets/Script/PlayerControl.cs
@@ -0,0 +1,145 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+using UnityEngine.XR.WSA.Input;
+
+public class PlayerControl : MonoBehaviour
+{
+    [Header("Control")]
+    public KeyCode Attack = KeyCode.J;
+    public KeyCode Jump = KeyCode.Space;
+    
+    [Header("Attribute")]
+    public float MovingSpeed;
+    public float JumpingSpeed;
+    public int ComboFrameRange;
+    public int MaxAttackStage;
+    public float HitRecover;
+    public LayerMask groundLayer;
+
+    private Vector2 SpeedH = new Vector2(0, 0);
+    private Vector2 SpeedV = new Vector2(0, 0);
+    private Rigidbody2D rb;
+
+    [Header("Game Stat")]
+    public Animator animator;
+    public bool FacingRight = true;
+    public int AttackStage;
+    public int ComboBar;
+    public bool injured;
+    public bool retreating;
+    public bool grounded;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        SpeedH.Set(MovingSpeed, 0);
+        SpeedV.Set(0, JumpingSpeed);
+    }
+    
+    void Update()
+    {
+        //Check for Grounded
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, transform.localScale.y * 0.6f, groundLayer);
+        if (hit.collider != null)
+        {
+            grounded = true;
+        }
+        else
+        {
+            grounded = false;
+        }        
+                
+        //Injured
+        if (injured && !retreating)
+        {
+            animator.SetBool("Injured", true);
+            rb.velocity *= -1;
+            if (rb.velocity.y > 0)
+            {
+               Vector2 magnify = rb.velocity;
+               magnify.y = JumpingSpeed;
+               rb.velocity = magnify;
+            }            
+            retreating = true;
+            Invoke("Recover", HitRecover);
+        }
+
+        if (!injured)
+        {
+            //Walk
+            rb.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * MovingSpeed, rb.velocity.y);
+            if (grounded && rb.velocity.x != 0)
+            {
+                animator.SetBool("Walking", true);
+            }
+            else
+            {
+                animator.SetBool("Walking", false);
+            } 
+                    
+            //Jump & Fall
+            animator.SetBool("Jump", false);
+            if (grounded && Input.GetKeyDown(Jump))
+            {
+                rb.velocity += SpeedV;
+                animator.SetBool("Jump", true);
+            }
+    
+            if (rb.velocity.y > 0)
+            {
+                animator.SetBool("Rising", true);
+            }
+            else
+            {
+                animator.SetBool("Rising", false);
+            } 
+            
+            if (rb.velocity.y < 0)
+            {
+                animator.SetBool("Falling", true);
+            }
+            else
+            {
+                animator.SetBool("Falling", false);
+            }
+            
+            //Flip
+            if (!injured && ((rb.velocity.x < 0 && FacingRight) || (rb.velocity.x > 0 && !FacingRight)))
+            {
+                FacingRight = !FacingRight;
+                Vector3 CharScale = transform.localScale;
+                CharScale.x *= -1;
+                transform.localScale = CharScale;        
+            }        
+        }
+
+        
+        //Attack        
+        if (ComboBar > 0)
+        {
+            ComboBar--;
+            if (ComboBar == 0)
+            {
+                AttackStage = 0;
+                animator.SetInteger("AttackStage", AttackStage);
+            }
+        }
+
+        if (!injured && Input.GetKeyDown(Attack) && AttackStage < MaxAttackStage && ComboBar < 30)
+        {
+            AttackStage++;
+            animator.SetInteger("AttackStage", AttackStage);
+            ComboBar += ComboFrameRange;        
+        }
+    }
+
+    void Recover()
+    {
+        injured = false;
+        animator.SetBool("Injured", false);
+        retreating = false;
+    }
+}
